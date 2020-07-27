1c0261c381a9865b08ce1e2ccde7f74db1c59f1e
Ralph Cacho <cacho.rc@gmail.com>
2020-07-26 19:28:00 -0700

    Add Semantic-UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "semantic-ui-css/semantic.min.css";
 
 import Navbar from "./components/navbar.component";
 import Home from "./components/home.component";
